6d60a048a4c9d5e86b537257d73b31029486015f
nicolas <fshqss@live.com>
2024-10-28 01:58:46 +0800

    button ok

--- GD32F10x_Firmware_Library_V2.3.0/Template/Keil5_project/button.c
+++ GD32F10x_Firmware_Library_V2.3.0/Template/Keil5_project/button.c
@@ -1,34 +1,55 @@
 #include "button.h"
+#include "gd32f10x.h"
 
+static rcu_periph_enum GPIO_CLK[BUTTONn] = {BUTTON1_GPIO_CLK, BUTTON2_GPIO_CLK, 
+																						BUTTON3_GPIO_CLK, BUTTON4_GPIO_CLK,
+																						BUTTON5_GPIO_CLK, BUTTON6_GPIO_CLK};
+
+static rcu_periph_enum GPIO_PORT[BUTTONn] = {BUTTON1_GPIO_PORT, BUTTON2_GPIO_PORT, 
+																						 BUTTON3_GPIO_PORT, BUTTON4_GPIO_PORT,
+																						 BUTTON5_GPIO_PORT, BUTTON6_GPIO_PORT};																						
+																						
+static rcu_periph_enum GPIO_PIN[BUTTONn] = {BUTTON1_PIN, BUTTON2_PIN, 
+																						BUTTON3_PIN, BUTTON4_PIN,
+																						BUTTON5_PIN, BUTTON6_PIN};
 
 /**
 	* button init function 
 	*/
-void button_init(void)
+void button_init(button_typedef_enum btnNum)
 {
+	/* enable the led clock */
+    rcu_periph_clock_enable(GPIO_CLK[btnNum]);
+    /* configure led GPIO port */ 
+    gpio_init(GPIO_PORT[btnNum], GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN[btnNum]);
 
+    GPIO_BC(GPIO_PORT[btnNum]) = GPIO_PIN[btnNum];
 }
 
-/**
-	* button release function 
-	*/
-void button_release(void)
+/*!
+    \brief      button release function 
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void button_release()
 {
-
+	gpio_deinit(GPIOB);
 }
 
 /**
 	* button read function 
 	*/
-void button_read(void)
+button_status_typedef_enum button_read(button_typedef_enum btnNum)
 {
-
+	FlagStatus status = gpio_input_bit_get(GPIO_PORT[btnNum], GPIO_PIN[btnNum]);
+	return status == SET ? BUTTON_DOWN : BUTTON_UP;
 }
 
 /**
 	* button set function 
 	*/
-void button_set(void)
+void button_set(button_typedef_enum btnNum)
 {
-
+	//none
 }
--- GD32F10x_Firmware_Library_V2.3.0/Template/Keil5_project/button.h
+++ GD32F10x_Firmware_Library_V2.3.0/Template/Keil5_project/button.h
@@ -1,16 +1,69 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include "gd32f10x.h"
+
+#define BUTTONn		6U
+
+/* exported types */
+typedef enum 
+{
+    BUTTON1 = 0,
+    BUTTON2 = 1,
+    BUTTON3 = 2,
+    BUTTON4 = 3,
+		BUTTON5 = 4,
+		BUTTON6 = 5,
+} button_typedef_enum;
+
+typedef enum 
+{
+    BUTTON_UP = 0,
+    BUTTON_DOWN = 1,
+    BUTTON_NONE   = 2
+} button_status_typedef_enum;
+
+typedef enum 
+{
+    KEY_MODE_GPIO = 0,
+    KEY_MODE_EXTI = 1
+} button_mode_typedef_enum;
+
+
+#define BUTTON1_PIN                         GPIO_PIN_1
+#define BUTTON1_GPIO_PORT                   GPIOB
+#define BUTTON1_GPIO_CLK                    RCU_GPIOB
+
+#define BUTTON2_PIN                         GPIO_PIN_3
+#define BUTTON2_GPIO_PORT                   GPIOB
+#define BUTTON2_GPIO_CLK                    RCU_GPIOB
+  
+#define BUTTON3_PIN                         GPIO_PIN_5
+#define BUTTON3_GPIO_PORT                   GPIOB
+#define BUTTON3_GPIO_CLK                    RCU_GPIOB
+  
+#define BUTTON4_PIN                         GPIO_PIN_7
+#define BUTTON4_GPIO_PORT                   GPIOB
+#define BUTTON4_GPIO_CLK                    RCU_GPIOB
+  
+#define BUTTON5_PIN                         GPIO_PIN_9
+#define BUTTON5_GPIO_PORT                   GPIOB
+#define BUTTON5_GPIO_CLK                    RCU_GPIOB
+
+#define BUTTON6_PIN                         GPIO_PIN_11
+#define BUTTON6_GPIO_PORT                   GPIOB
+#define BUTTON6_GPIO_CLK                    RCU_GPIOB
+
 /* button init function */
-void button_init(void);
+void button_init(button_typedef_enum btnNum);
 
 /* button release function */
-void button_release(void);
+void button_release();
 
 /* button read function */
-void button_read(void);
+button_status_typedef_enum button_read(button_typedef_enum btnNum);
 
 /* button set function */
-void button_set(void);
+void button_set(button_typedef_enum btnNum);
 
 #endif /* BUTTON_H */
